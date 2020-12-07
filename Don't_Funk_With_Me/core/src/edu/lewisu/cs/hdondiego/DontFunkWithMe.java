3ba1bfbde353fb35f357ade96b6f2cb4ecee2fe8
hecjdondiego@gmail.com <hecjdondiego@gmail.com>
2020-12-07 12:44:32 -0600

    Added Sprite and animation

--- Don't_Funk_With_Me/core/src/edu/lewisu/cs/hdondiego/DontFunkWithMe.java
+++ Don't_Funk_With_Me/core/src/edu/lewisu/cs/hdondiego/DontFunkWithMe.java
@@ -6,6 +6,7 @@ import edu.lewisu.cs.cpsc41000.common.labels.*;
 import edu.lewisu.cs.cpsc41000.common.labels.ActionLabel;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,9 +14,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DontFunkWithMe extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture theKingMap;
+	Texture theKingMap, player1Tex; // player2Tex
+	AnimatedImageBasedScreenObject player1; // player2
+	ImageBasedScreenObjectDrawer drawer1;
 	OrthographicCamera cam, titleCam;
 	int WIDTH, HEIGHT;
+	int[] player1Seq = {0,0,1,0,2,0,0,1,1,1,2,1};
 	ActionLabel player1Label, player2Label;
 	
 	@Override
@@ -28,20 +32,45 @@ public class DontFunkWithMe extends ApplicationAdapter {
 		cam.translate(WIDTH/2, HEIGHT/2);
 		cam.update();
 		batch.setProjectionMatrix(cam.combined);
-		
+		drawer1 = new ImageBasedScreenObjectDrawer(batch);
 		player1Label = new ActionLabel("Johnny Dansalot", 10, 630, "fonts/agency_fb_35pt_black.fnt");
 		player2Label = new ActionLabel("The King", 820, 630, "fonts/agency_fb_35pt_black.fnt");
+		player1Tex = new Texture("johnny_dansalot.png");
+		/*
+		 public AnimatedImageBasedScreenObject(Texture tex, int xpos, int ypos, int xorigin, 
+    int yorigin, int rotation, int scaleX, int scaleY, boolean flipX, boolean flipY,
+    float frameWidth, float frameHeight, int[] frameSequence, float animDelay)
+		 */
+		player1 = new AnimatedImageBasedScreenObject(player1Tex, 600, 130, 0, 0, 0, 10, 10, false, false, 32f, 32f, player1Seq, 0.1f);
+		
 	}
 
 	@Override
 	public void render () {
+		float dt = Gdx.graphics.getDeltaTime();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// move to the right
+		if (Gdx.input.isKeyPressed(Keys.D)) {
+			player1.animate(dt);
+			player1.setXPos(player1.getXPos() + 5);
+		}
+		
+		// move to the left
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			player1.animate(dt);
+			player1.setXPos(player1.getXPos() - 5);
+		}
+		
+		
+		
 		batch.begin();
 		//player1Label.draw(batch, 1);
 		batch.draw(theKingMap, 0, 0);
 		player1Label.draw(batch, 1);
 		player2Label.draw(batch, 1);
+		drawer1.draw(player1);
 		batch.end();
 	}
 	
